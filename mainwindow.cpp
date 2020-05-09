96aa99ede8e02d49ded0a8de42775729c373a083
ahmedalaa11 <47926165+ahmedalaa11@users.noreply.github.com>
2020-05-10 01:47:46 +0200

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_OpenFileButton_clicked()
+{
+    QFile input_file(QFileDialog::getOpenFileName(this,tr("Open File"),"",tr("XML File (*.XML) ;;TextFile (*.txt)")));
+    input_file.open(QIODevice::ReadOnly |QIODevice::Text);
+    QTextStream stream(&input_file);
+    QString text= stream.readAll();
+    ui->input_text->setText(text);
+   input_file.close();
+}
